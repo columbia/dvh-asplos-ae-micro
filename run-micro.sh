7670e7f1694f93c5d3141a47b3e56e03265d6c9d
Jintack Lim <jintack@cs.columbia.edu>
2017-03-15 11:27:47 -0600

    Setup ipi micro test (cycles)
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-micro.sh
+++ run-micro.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
+rm -f result
 ./x86-run x86/vmexit.flat | tee result
 for i in `seq 1 9`;
 do
 	./x86-run x86/vmexit.flat | tee -a result
 done
-echo "vmcall avg"
-grep vmcall result | awk '{ print $3 }'
-echo "vmcall min"
-grep vmcall result | awk '{ print $5 }'
-echo "outl_to_kernel avg"
-grep outl_to_kernel result | awk '{ print $3 }'
-echo "outl_to_kernel min"
-grep outl_to_kernel result | awk '{ print $5 }'
+echo "ipi avg"
+grep ipi: result | awk '{ print $3 }'
+echo "ipi min"
+grep ipi: result | awk '{ print $5 }'
