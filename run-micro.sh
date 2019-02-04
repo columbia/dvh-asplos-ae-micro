f1ec00befb197b8cb028158cf72498256cc26098
Jintack Lim <jintack@cs.columbia.edu>
2019-02-04 09:33:44 -0600

    Set up QEMU path explicitly
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- run-micro.sh
+++ run-micro.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 rm -f result
 APPEND=''
+
+L0_QEMU=/srv/vm/qemu/x86_64-softmmu/qemu-system-x86_64
+L1_QEMU=/root/vm/qemu/x86_64-softmmu/qemu-system-x86_64
+
+#Check if we are on a bare-metal machine
+uname -n | grep -q cloudlab
+err=$?
+
+if [[ $err == 0 ]]; then
+	MY_QEMU=$L0_QEMU
+else
+	MY_QEMU=$L1_QEMU
+fi
+
 for i in `seq 0 9`;
 do
 	if [ i != 0 ]; then
 		APPEND="-a"
 	fi
-	./x86-run x86/vmexit.flat | tee $APPEND result
+	QEMU=$MY_QEMU ./x86-run x86/vmexit.flat | tee $APPEND result
 done
+
 echo "ipi avg"
 grep ipi: result | awk '{ print $3 }'
 echo "ipi min"
