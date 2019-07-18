961d4e2ac6da2b99a12e68445137a358300e4c17
Jintack Lim <jintack@cs.columbia.edu>
2019-07-17 22:11:56 -0600

    Add sample command for timer test

--- run-micro.sh
+++ run-micro.sh
@@ -19,6 +19,9 @@ fi
 
 #Run this command for timer measure
 #./x86-run x86/tscdeadline_wr_latency.flat
+# This is for 100ms, 100 iterations
+#./x86-run x86/tscdeadline_wr_latency.flat --append "220000000 100"
+#QEMU=/root/vm/qemu-pi/x86_64-softmmu/qemu-system-x86_64
 for i in `seq 0 9`;
 do
 	if [ i != 0 ]; then
